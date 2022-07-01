6fa4f5008603bcc86fb59ef94132d6683bdf0f39
Oshan Shrestha <os02411418@student.ku.edu.np>
2022-07-01 14:29:35 +0545

    some bug fixes

--- routers/auth.py
+++ routers/auth.py
@@ -11,25 +11,31 @@ from utils.token import get_access_token
 
 router = APIRouter(prefix="/auth")
 
+_COOKIE_EXPIRE_DELTA=20 # minutes
+
 @router.get("/")
 def home():
     return {'check': True}
 
-@router.get("/refresh", response_model=User)
+@router.get("/token", response_model=User)
 def refresh(response: Response, payload: dict = Depends(validate_session)):
-    payload.pop("exp")
+    if "exp" in payload:
+        payload.pop("exp")
     token = get_access_token(payload=payload)
     response.set_cookie("session", token, max_age=timedelta(minutes=2), httponly=True)
     return payload
 
 @router.post("/register", response_model=User, dependencies=[Depends(captcha.validateCaptcha)])
-async def register(user: UserCreateBase, db: Session = Depends(db.get_db)):
+async def register(user: UserCreateBase, response: Response, db: Session = Depends(db.get_db)):
     user_exists = get_user_by_email(db, user.email)
     if user_exists:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists.")
-    return create_new_user(db, user)
+    new_user = create_new_user(db, user)
+    token = get_access_token(new_user.to_dict())
+    response.set_cookie("session", token, max_age=timedelta(minutes=_COOKIE_EXPIRE_DELTA), httponly=True)
+    return new_user
 
-@router.post("/login", response_model=User, dependencies=[Depends(captcha.validateCaptcha)])
+@router.post("/signin", response_model=User, dependencies=[Depends(captcha.validateCaptcha)])
 async def login(user: UserLoginBase, response : Response, db: Session = Depends(db.get_db)):
     user_exists = get_user_by_email(db, user.email)
     if not user_exists:
@@ -37,5 +43,10 @@ async def login(user: UserLoginBase, response : Response, db: Session = Depends(
     if not verify_password(user_exists.password, user.password):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Credentials invalid.")
     token = get_access_token(user_exists.to_dict())
-    response.set_cookie("session", token, max_age=timedelta(minutes=2), httponly=True)
+    response.set_cookie("session", token, max_age=timedelta(minutes=_COOKIE_EXPIRE_DELTA), httponly=True)
     return user_exists
+
+@router.post("/signout", dependencies=[Depends(validate_session)])
+def logout(response: Response):
+    response.delete_cookie("session")
+    return dict(success=True)
--- utils/token.py
+++ utils/token.py
@@ -4,7 +4,7 @@ from jose import jwt
 _SECRET_KEY="28d5337052696989ce5da78631a25e9727a4595868a0bd0cd79bb6f75c852f12"
 _ALGORITHM="HS512"
 
-def get_access_token(payload: dict, expire_delta: int | None) -> str:
+def get_access_token(payload: dict, expire_delta: int | None = None) -> str:
     _payload = payload.copy()
     if expire_delta:
         _payload.update({"exp": datetime.utcnow() + expire_delta}) # 2 minutes
